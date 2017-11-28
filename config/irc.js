5b48f03e9f117eba674b4f79e7a62531bb20b65a
Darren <darren@darrenwhitlen.com>
2017-11-28 16:09:31 +0000

    Updating config file

--- config/irc.js
+++ config/irc.js
@@ -1,6 +1,9 @@
 module.exports =  {
   CHANNEL: '#handshake',
   SERVER: 'chat.freenode.net',
+  PORT: 6667,
+  TLS: false,
   BOTNAME: 'ProjectHandShake',
   PASSWORD: 'CUFTN4ausiGXBbpCgkkWPajk',
+  NICKINFOTIMEOUT: 20000,
 }
